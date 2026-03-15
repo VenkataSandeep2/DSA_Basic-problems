dd234f8074fade6e23f7f88f0461fe19aa1b7d19
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-03-15 23:53:46 +0530

    Update Reversestring1.java

--- Basic/Reversestring1.java
+++ Basic/Reversestring1.java
@@ -2,15 +2,15 @@ class Reversestring1
 {
 public static void main(String args[])
 	{
-	String s = "RamGopal Varma";
+	String s = "Ram Gopal Varma";
 	String s1="";
-	System.out.println(s.length());
-	for(int i=13  ; i>=0 ; i--)
+	System.out.println("The length of the string is :- "+s.length());
 	{
+	for(int i=13  ; i>=0 ; i--)
 	s1 = s1 + s.charAt(i);
 	}
+	System.out.println("OUTPUT");
 	System.out.println(s1);
-	System.out.println(s1.length());
 	
 	}
 }
