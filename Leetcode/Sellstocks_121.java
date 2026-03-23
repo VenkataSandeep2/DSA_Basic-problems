4fb5539278b229aa77bdb898728f65fb3ff2df0d
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-03-23 23:18:08 +0530

    Update Sellstocks_121.java

--- Leetcode/Sellstocks_121.java
+++ Leetcode/Sellstocks_121.java
@@ -23,3 +23,24 @@ public class Sellstocks_121
 }
 	
 
+			/*	or 
+			
+						*/
+public class Sellstocks_121 
+{
+	public static void main(String args[])
+	{        int prices[] = {7,1,5,3,6,4};
+        int m = Integer.MAX_VALUE;
+        int profit  =0;
+        int n =prices.length;
+        for(int i =0 ;i<n-1 ; i++)
+        {
+            for(int j =i+1;j<n;j++)
+            {
+                profit = Math.max(profit,prices[j]-prices[i]);
+                
+            }
+        }
+        return profit;
+    }
+}
