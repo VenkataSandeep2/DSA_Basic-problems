4a8ad6a03b53d56dd7f069fa80778c82c6d2b1e5
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-25 22:04:32 +0530

    Update Primenumber2.java

--- Basic/prime & palindrome/Primenumber2.java
+++ Basic/prime & palindrome/Primenumber2.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Primenumber2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a number: ");
+		System.out.print("Enter a number:- ");
 		int num = sc.nextInt();
 		boolean isPrime = true;
 		if(num <= 1)
