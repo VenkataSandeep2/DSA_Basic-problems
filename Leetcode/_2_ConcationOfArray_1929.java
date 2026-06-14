578441b36c9d92ea0da3ba9fd26013fb954b0f5a
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-06-14 21:43:24 +0530

    Create _2_ConcationOfArray_1929.java

---
+++ Leetcode/_2_ConcationOfArray_1929.java
@@ -0,0 +1,17 @@
+class _2_ConcationOfArray_1929 
+{
+    public int[] getConcatenation(int[] nums) 
+    {
+        int n =nums.length;
+        int[] ans =new int[2*n];
+
+        for(int i =0;i<n;i++)
+        {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i]; 
+        }
+        return ans;
+
+        
+    }
+}
