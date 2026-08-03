334d03bae53969da03a0b907a0a18004f93d60a3
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-08-03 23:19:30 +0530

    Create LargestNumer_02.java

---
+++ Interivew_Questions/LargestNumer_02.java
@@ -0,0 +1,19 @@
+package com.InterviewQuestions;
+
+public class LargestNumber_02 
+{
+	 public static void main(String args[])
+	    {
+	      int arr[] = {10,0,80,96,56,4465,46,465,465,9999,45,465,4,46,4,65};
+	      int in;
+	      int max = arr[0];
+
+	      for(int i=0;i<arr.length-1;i++)
+	        {
+	          if(arr[i] > max)
+	        	  max=arr[i];
+	            
+	        }
+	      System.out.println(max);
+	    }
+}
