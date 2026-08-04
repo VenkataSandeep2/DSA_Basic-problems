71f88084ba338f418e17945ecbaa7d0aba8b5fba
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-08-04 23:07:52 +0530

    Create __003_SecondLargest.java

---
+++ Interivew_Questions/__003_SecondLargest.java
@@ -0,0 +1,27 @@
+package com.InterviewQuestions;
+
+public class __003_SecondLargest
+{
+	public static void main(String[] args) {
+		int arr[] = {10,03,50,587,989,100,654,5,888,53,15,99,85};
+		int largest = Integer.MIN_VALUE;
+		int SecLargest = Integer.MAX_VALUE;
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i] > largest)
+			{
+				SecLargest = largest;
+				largest = arr[i];
+			}
+			else if (arr[i] > SecLargest && arr[i] != largest)
+			{
+				SecLargest = arr[i];
+				
+			}
+		}
+		System.out.println("Largest number -->" +largest);
+		System.out.println("Second Largest -->  "+SecLargest);
+	}
+
+}
